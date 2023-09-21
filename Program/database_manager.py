5f02b1b757e9c3beed339d7ff5c12f9afe0d54e3
wsq94317 <wsq94317@vip.qq.com>
2023-09-21 21:36:10 +1000

    Part B Create Python file main.py database_manager.py

---
+++ Program/database_manager.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self):
+        self.database = None
+        self.cursor = None
+        self.init_database()
+
+    def init_database(self):
+        try:
+            self.database = sqlite3.connect("./database/airbnbdata.db")
+            self.cursor = self.database.cursor()
+        except sqlite3.Error as e:
+            print(f"Database error {e}")
+            return
+        except Exception as e:
+            print(f"General error {e}")
+            return
+
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='listings';")
+
+        if not self.cursor.fetchone():
+            self.fetch_data_from_csv()
+
+    def fetch_data_from_csv(self):
+        pass
+
+    def get_database(self):
+        return self.cursor
+
+
+    def close(self):
+        if self.database:
+            self.database.close()
---
+++ Program/main.py
@@ -0,0 +1,12 @@
+import sqlite3
+from database_manager import DatabaseManager
+
+def main():
+    # 1. load data from Database manager
+    db_manager = DatabaseManager()
+    db = db_manager.get_database()
+    if not db:
+        return
+
+if __name__ == "__main__":
+    main()
