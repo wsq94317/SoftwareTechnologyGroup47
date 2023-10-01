015cbba8349c1d6e0e3e694c55e7462212222258
ChienHuang0818 <betty556611@gmail.com>
2023-10-01 21:34:49 +1000

    modify my function2

--- Program/deffunction2.py
+++ Program/deffunction2.py
@@ -3,30 +3,44 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 def query_price_distribution_data(date, total_days):
-    # 连接数据库
+    # Connect to the database
     database = sqlite3.connect("database/airbnbdata.db")
 
-    # 计算结束日期
+    # Calculate the end date for the query
     end_date = (datetime.strptime(date, "%Y-%m-%d") + timedelta(days=total_days)).strftime("%Y-%m-%d")
 
-    # 构建价格查询语句
+    # Build the price query
     price_query = """SELECT price FROM Calendar
                      WHERE date BETWEEN ? AND ?;"""
 
-    # 执行价格查询
+    # Execute the price query
     price_cursor = database.execute(price_query, (start_date, end_date))
 
-    # 提取价格数据，并过滤掉空值
+    # Extract price data and filter out null values
     prices = [row[0] for row in price_cursor.fetchall() if row[0] is not None]
 
-    print(max(prices))
+    # Print statistics about the prices
+    print("Maximum Price:", max(prices))
+    print("Number of Prices:", len(prices))
+    print("Minimum Price:", min(prices))
 
-    # 关闭数据库连接
+    # Close the database connection
     database.close()
 
-    price_bins = [price for price in range(100, 15000, 500)]
+    # Find the minimum and maximum values in the prices list
+    min_price = min(prices)
+    max_price = max(prices)
 
-    # 绘制直方图
+    # Calculate the bin width for the histogram
+    bin_width = (max_price - min_price) / 10
+
+    # Create price bins
+    price_bins = [min_price + i * bin_width for i in range(11)]
+
+    # Convert the bins to integers (if needed)
+    price_bins = [int(bin) for bin in price_bins]
+
+    # Plot a histogram of the prices
     plt.hist(prices, bins=price_bins, edgecolor='k')
     plt.title("Price Distribution")
     plt.xlabel("Price")
@@ -34,11 +48,19 @@ def query_price_distribution_data(date, total_days):
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.savefig("price_distribution.png")
 
+    # Add text labels above each bar showing the count
+    n, bins, patches = plt.hist(prices, bins=price_bins, edgecolor='k')
+    for i, count in enumerate(n):
+        if count > 0:
+            plt.text(bins[i] + (bins[i + 1] - bins[i]) / 2, count, str(int(count)), ha='center', va='bottom')
+
+    # Return the plot
     return plt
 
+
 # 示例用法
-start_date = "2019-05-30"
-num_days = 10
+start_date = "2019-05-11"
+num_days = 9
 prices = query_price_distribution_data(start_date, num_days)
 plt.show()
 
