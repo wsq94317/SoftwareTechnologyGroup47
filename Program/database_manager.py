19bc158db33df4db0977259e2599a0bcc8214ec8
ChienHuang0818 <betty556611@gmail.com>
2023-10-01 21:32:32 +1000

    I add my finall function1&2 in there

--- Program/database_manager.py
+++ Program/database_manager.py
@@ -1,6 +1,8 @@
 import sqlite3
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 class DatabaseManager:
     def __init__(self):
@@ -191,13 +193,109 @@ class DatabaseManager:
         self.close()
         return min_year,max_year
 
-    def query_location_data(self, surburb_list, date, total_days):
-        print(surburb_list, date, total_days)
-        pass
+    def query_location_data(self,suburb_list, date, total_days):
+        # Connect to the database
+        self.connect_database()
 
-    def query_price_distribution_data(self, date, total_days):
-        print(date, total_days)
-        pass
+        # Calculate the end date for the query
+        end_date = (datetime.strptime(date, "%Y-%m-%d") + timedelta(days=total_days)).strftime("%Y-%m-%d")
+
+        # Build the date query using the BETWEEN condition to match date range
+        date_query = """SELECT DISTINCT c.listing_id FROM Calendar c
+                        WHERE c.available = ? AND c.date BETWEEN ? AND ?;"""
+
+        # Build date query parameters, including start date and end date
+        date_params = [1, date, end_date]
+
+        # Execute the date query
+        date_cursor = self.cursor.execute(date_query, date_params)
+
+        # Get the date query result
+        date_result = date_cursor.fetchall()
+
+        # Batch size for querying
+        batch_size = 1000
+
+        # Initialize the result list
+        suburb_result = []
+
+        # Process date results in batches
+        for i in range(0, len(date_result), batch_size):
+            batch_date_result = date_result[i:i + batch_size]
+
+            # Build the suburb query to find listings matching dates and connect to Suburb table to get suburb names
+            suburb_query = """SELECT h.* FROM House h
+                             INNER JOIN Surburb s ON h.surburb_id = s.surburb_id
+                             WHERE h.id IN ({})
+                             AND s.surburb_name = ? ;""".format(', '.join(['?'] * len(batch_date_result)))
+
+            # Build suburb query parameters, including date list and suburb name
+            suburb_params = tuple([param[0] for param in batch_date_result] + [suburb_list])
+
+            # Execute the suburb query
+            suburb_cursor = self.cursor.execute(suburb_query, suburb_params)
+
+            # Get and extend the suburb query results to the result list
+            suburb_result.extend(suburb_cursor.fetchall())
+
+        # Return the suburb query result
+        print(suburb_result)
+
+        # Close the database connection
+        self.close()
+
+    def query_price_distribution_data(self,date, total_days):
+        # Connect to the database
+        self.connect_database()
+
+        # Calculate the end date for the query
+        end_date = (datetime.strptime(date, "%Y-%m-%d") + timedelta(days=total_days)).strftime("%Y-%m-%d")
+
+        # Build the price query
+        price_query = """SELECT price FROM Calendar
+                         WHERE date BETWEEN ? AND ?;"""
+
+        # Execute the price query
+        price_cursor = self.cursor.execute(price_query, (date, end_date))
+
+        # Extract price data and filter out null values
+        prices = [row[0] for row in price_cursor.fetchall() if row[0] is not None]
+
+        # Close the database connection
+        self.close()
+
+        # Find the minimum and maximum values in the prices list
+        min_price = min(prices)
+        max_price = max(prices)
+
+        # Calculate the bin width for the histogram
+        bin_width = (max_price - min_price) / 10
+
+        # Create price bins
+        price_bins = [min_price + i * bin_width for i in range(11)]
+
+        # Convert the bins to integers (if needed)
+        price_bins = [int(bin) for bin in price_bins]
+
+        # Plot a histogram of the prices
+        plt.hist(prices, bins=price_bins, edgecolor='k')
+        plt.title("Price Distribution")
+        plt.xlabel("Price")
+        plt.ylabel("Count")
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+        plt.savefig("price_distribution.png")
+
+        # Add text labels above each bar showing the count
+        n, bins, patches = plt.hist(prices, bins=price_bins, edgecolor='k')
+        for i, count in enumerate(n):
+            if count > 0:
+                plt.text(bins[i] + (bins[i + 1] - bins[i]) / 2, count, str(int(count)), ha='center', va='bottom')
+
+        # Display the plot
+        plt.show()
+
+        # Return the plot
+        return plt
 
     def query_kword_data(self, date, total_days, keyword):
         self.connect_database()
